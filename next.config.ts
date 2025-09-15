a1040af812e629d0887b0f82ab9c4cd58e2f8b5b
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-09-15 18:45:54 +0200

    feat: add i18n

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin("./src/shared/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig)
---
+++ src/shared/api/queries/queries.ts
@@ -0,0 +1,47 @@
+import { gql } from '@apollo/client'
+
+export const LOGIN_ADMIN = gql`
+    mutation LoginAdmin($email: String!, $password: String!) {
+        loginAdmin(email: $email, password: $password) {
+            logged
+        }
+    }
+`
+
+export const GET_USERS_QUERY = gql`
+    query GetUsers(
+        $pageNumber: Int = 1
+        $pageSize: Int = 10
+        $sortBy: String = "createdAt"
+        $sortDirection: SortDirection = desc
+        $searchTerm: String
+        $statusFilter: UserBlockStatus = ALL
+    ) {
+        getUsers(
+            pageNumber: $pageNumber
+            pageSize: $pageSize
+            sortBy: $sortBy
+            sortDirection: $sortDirection
+            searchTerm: $searchTerm
+            statusFilter: $statusFilter
+        ) {
+            users {
+                id
+                userName
+                email
+                createdAt
+                profile {
+                    createdAt
+                    firstName
+                    lastName
+                    id
+                }
+            }
+            pagination {
+                page
+                pageSize
+                totalCount
+            }
+        }
+    }
+`
