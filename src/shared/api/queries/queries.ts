58eb4393cb8f33bff4ccd6f548322e7dfc48fbdd
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-09-16 17:10:05 +0200

    feat: add ban/unban logic

--- src/shared/api/queries/queries.ts
+++ src/shared/api/queries/queries.ts
@@ -50,3 +50,14 @@ export const GET_USERS_QUERY = gql`
         }
     }
 `
+
+export const BAN_USER = gql`
+mutation banUser($banReason: String!, $userId: Int!) {
+    banUser(banReason: $banReason,userId:$userId)
+}`
+
+export const UNBAN_USER = gql`
+    mutation unbanUser( $userId: Int!) {
+        unbanUser(userId:$userId),
+        
+    }`
--- src/shared/ui/DropDown/DropDown.tsx
+++ src/shared/ui/DropDown/DropDown.tsx
@@ -4,13 +4,24 @@ import MoreHorizontal from '@/shared/assets/icons/components/MoreHorizontal'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import s from './Dropdown.module.scss'
 import { User } from '@/shared/api/types'
+import { useState } from 'react'
+import { ActionModal } from '@/shared/ui/Button/ActionModal'
+
+
+export type modalType = null | 'delete' | 'ban' | 'unban'
 
 type Props = {
   item: User
   onEditModeToggle?: () => void
-  labels: string[]
 }
-export function Dropdown({ item,labels, onEditModeToggle }: Props) {
+
+export function Dropdown({ item}: Props) {
+  const [modalType, setModalType] = useState<modalType>(null)
+  const handleOpenModal = (type: 'delete' | 'ban' | 'unban') => {
+    setModalType(type)
+  }
+
+
   return (
     <>
       <DropdownMenu.Root modal={false}>
@@ -25,15 +36,25 @@ export function Dropdown({ item,labels, onEditModeToggle }: Props) {
           sideOffset={8}
           align="end"
         >
-          {labels.map((label,i) => (
-            <DropdownMenu.Item className={s.menuItem} key={i}>
-              <div className={s.item} >
-                {label}
-              </div>
-            </DropdownMenu.Item>
-          ))}
+          <DropdownMenu.Item className={s.menuItem} onClick={() => handleOpenModal('delete')}>
+            <div className={s.item}>
+              Delete User
+            </div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className={s.menuItem} onClick={() => handleOpenModal(item.userBan ? 'unban' : 'ban')}>
+            <div className={s.item}>
+              {item.userBan ? 'Unban account' : 'Ban in the system'}
+            </div>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className={s.menuItem}>
+            <div className={s.item}>
+              More information
+            </div>
+          </DropdownMenu.Item>
         </DropdownMenu.Content>
       </DropdownMenu.Root>
+      <ActionModal modalType={modalType} userId={item.id} isBan={!!item.userBan} userName={item.userName}
+                   onClose={setModalType} />
     </>
   )
 }
