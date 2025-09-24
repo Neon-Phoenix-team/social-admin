faf0141fab3ed90a3f2390b60f94100b00c90b6a
lopatop <pavellopato327@gmail.com>
2025-09-24 23:05:29 +0200

    added: sorted function

--- src/features/UsersList/UsersList.tsx
+++ src/features/UsersList/UsersList.tsx
@@ -2,22 +2,29 @@
 
 import { GET_USERS_QUERY } from '@/shared/api/queries/queries'
 import { useQuery } from '@apollo/client/react'
-import {
-  GetUsersQuery,
-  GetUsersQueryVariables,
-} from '@/shared/api/queries/queries.generated'
+import { GetUsersQuery, GetUsersQueryVariables } from '@/shared/api/queries/queries.generated'
 import s from './UsersList.module.scss'
 import { Dropdown } from '@/shared/ui/DropDown/DropDown'
 import { Pagination } from '@/shared/ui/Pagination/Pagination'
 import { useState } from 'react'
 import { useTranslations } from 'next-intl'
 import Block from '@/shared/assets/icons/components/dropDown/Block'
+import Filter from '@/shared/assets/icons/common/Filter'
+import { Button } from '@/shared/ui/Button/Button'
+import { SortConfigType, SortDirection } from '@/shared/api/types'
+import SortAsc from '@/shared/assets/icons/common/SortAsc'
+import SortDesc from '@/shared/assets/icons/common/SortDesc'
+import { LinearProgress } from '@/shared/ui/LinearProgress/LinearProgress'
+
 
 export const UsersList = () => {
   const [page, setPage] = useState(1)
   const [itemsCountForPage, setItemsCountForPage] = useState(8)
+  const [sortConfig, setSortConfig] = useState<SortConfigType>(
+    { sortBy: 'createdAt', sortDirection: SortDirection.Desc})
+
   const { data, loading } = useQuery<GetUsersQuery, GetUsersQueryVariables>(GET_USERS_QUERY, {
-    variables: { pageNumber: page, pageSize: itemsCountForPage },
+    variables: { pageNumber: page, pageSize: itemsCountForPage, sortBy: sortConfig.sortBy, sortDirection: sortConfig.sortDirection },
   })
 
   const t = useTranslations('userList')
@@ -27,7 +34,34 @@ export const UsersList = () => {
     setItemsCountForPage(args.count)
   }
 
-  if (loading) return <div>Loading...</div>
+  const getSortIcon = (columnName: string) => {
+    if (sortConfig.sortBy === columnName) {
+      if (sortConfig.sortDirection === SortDirection.Asc) {
+        return <SortAsc />;
+      } else {
+        return <SortDesc />;
+      }
+    }
+    return <Filter />;
+  };
+
+
+
+  const handleSort = (columnName: string) => {
+    setSortConfig(prevConfig => {
+      if (prevConfig.sortBy === columnName) {
+        return {
+          sortBy: columnName,
+          sortDirection: prevConfig.sortDirection === SortDirection.Asc
+            ? SortDirection.Desc
+            : SortDirection.Asc,
+        }
+      }
+      return { sortBy: columnName, sortDirection: SortDirection.Asc }
+    })
+  }
+
+  if (loading) return <div><LinearProgress/></div>
 
 
   return (
@@ -37,8 +71,23 @@ export const UsersList = () => {
         <tr>
           <th>{t('userId')}</th>
           <th>{t('profileLink')}</th>
-          <th>{t('userName')}</th>
-          <th>{t('dateAdded')}</th>
+          <th>
+            <div className={s.thWrapper}>
+              {t('userName')}
+              <Button variant={'text'} onClick={()=>{handleSort('userName')}} style={{ padding: '6px' }}>
+                {getSortIcon('userName')}
+              </Button>
+            </div>
+
+          </th>
+          <th>
+            <div className={s.thWrapper}>
+              {t('dateAdded')}
+              <Button variant={'text'} onClick={()=>{handleSort('createdAt')}} style={{ padding: '6px' }}>
+                {getSortIcon('createdAt')}
+              </Button>
+            </div>
+          </th>
           <th></th>
         </tr>
         </thead>
