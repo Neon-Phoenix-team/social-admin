97e6aaf4290c3a6c37322869c40e6cf24ddd1810
lopatop <pavellopato327@gmail.com>
2025-10-02 21:50:17 +0200

    added: Constants

--- src/features/UserInformation/lib/Constants/Constants.tsx
+++ src/features/UserInformation/lib/Constants/Constants.tsx
@@ -1,27 +1,31 @@
 import { Translate } from '@/shared/ui/Menu/menuData'
 import { TabItem } from '@/shared/ui/Tabs/Tabs'
+import { UploadedPhotos } from '@/features/UserInformation/ui/UploadedPhotos/UploadedPhotos'
+import { Payments } from '@/features/UserInformation/ui/Payments/Payments'
+import { Followers } from '@/features/UserInformation/ui/Followers/Followers'
+import { Following } from '@/features/UserInformation/ui/Following/Following'
 
 export function getSettingsSections(t: Translate): TabItem[] {
   return [
     {
       id: 'uploadedPhotos',
       label: t('uploadedPhotos'),
-      content: <UploadedPhotos />,
+      content: <UploadedPhotos/>,
     },
     {
       id: 'payments',
       label: t('payments'),
-      content: <Payments />,
+      content: <Payments/>,
     },
     {
       id: 'followers',
       label: t('followers'),
-      content: <Followers />,
+      content: <Followers/>,
     },
     {
       id: 'following',
       label: t('following'),
-      content: <Following />,
+      content: <Following/>,
     },
   ]
 }
