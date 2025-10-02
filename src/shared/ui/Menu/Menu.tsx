4dcdc293230e390a1e220970b4ef932be384d3cb
lopatop <pavellopato327@gmail.com>
2025-10-02 21:54:11 +0200

    fix: Menu fixed

--- src/shared/ui/Menu/Menu.tsx
+++ src/shared/ui/Menu/Menu.tsx
@@ -1,22 +1,29 @@
 
 import s from './Menu.module.scss'
 import { MenuItem, MenuItemPropsType } from './MenuItem/MenuItem'
+import { usePathname} from 'next/navigation'
 
 export const Menu = ({ items }: { items: MenuItemPropsType[] }) => {
-  
+
+  const pathname = usePathname()
+  const shouldHide = pathname.includes('/information')
+
+
   return (
-    <aside className={s.aside}>
-      <ul className={s.list}>
-        {items.map((item, index) => {
-          return (
-            <MenuItem
-              {...item}
-              key={index}
-              className={index === 4 ? s.extraSpace : ''}
-            />
-          )
-        })}
-      </ul>
-    </aside>
-  )
+    !shouldHide && (
+      <aside className={s.aside}>
+        <ul className={s.list}>
+          {items.map((item, index) => {
+            return (
+              <MenuItem
+                {...item}
+                key={index}
+                className={index === 4 ? s.extraSpace : ''}
+              />
+            )
+          })}
+        </ul>
+      </aside>
+    )
+)
 }
